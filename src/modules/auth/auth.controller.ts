e23fc43f10223c57248f767cb6532f8dbf9ae2aa
breezo <breezojr@gmail.com>
2022-11-09 12:18:32 +0300

    ♻️ auth refactor

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,21 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { LoginRequestDto } from './dtos/login.request';
 
-@Controller()
+@Controller('/auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    @UseGuards(AuthGuard('local'))
-    @Post('auth/login')
-    async login(@Request() req) {
-        return this.authService.login(req.user);
+    // @UseGuards(AuthGuard('local'))
+    @Post('/login')
+    async login(@Body() loginRequestBody: LoginRequestDto) {
+        return this.authService.login(loginRequestBody);
     }
 
-
     @UseGuards(JwtAuthGuard)
-    @Get('profile')
+    @Get('/profile')
     getProfile(@Request() req) {
         return req.user;
     }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import {  HttpException, HttpStatus,Injectable, Logger, NotAcceptableException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../user/providers/users.service';
+import { LoginRequestDto } from './dtos/login.request';
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly usersService: UsersService, 
         private jwtService: JwtService) { }
-        
+
+        logger = new Logger(AuthService.name)
+
     async validateUser(username: string, password: string): Promise<any> {
         const user = await this.usersService.getUser({ username });
         if (!user) return null;
@@ -21,10 +24,24 @@ export class AuthService {
         }
         return null;
     }
-    async login(user: any) {
-        const payload = { username: user.username, sub: user._id };
-        return {
-            access_token: this.jwtService.sign(payload),
-        };
+    async login(body: LoginRequestDto) {
+        const user = await this.usersService.getUser({ email: body.email});
+        if (!user) {
+            throw new NotAcceptableException('wrong email');
+        }
+        
+        const passwordValid = await bcrypt.compare(body.password, user.password)
+        if (!passwordValid) {
+            throw new NotAcceptableException('wrong password');
+        }
+
+        if (user && passwordValid) {
+            const payload = { username: body.email};
+            return  {
+                access_token: this.jwtService.sign(payload),
+            };
+        }
+
+        throw new HttpException('Login fail', HttpStatus.BAD_REQUEST )
     }
 }
