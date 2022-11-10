689518e02d1304549f72a6e28421e5742c7b0a05
breezo <breezojr@gmail.com>
2022-11-10 10:12:36 +0300

    ♻️  General Code refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { BookModule } from './modules/books/book.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/authentication'),
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     UserModule,
     AuthModule,
     BookModule
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  const logger = new Logger('main')
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = parseInt(process.env.SERVER_PORT) || 3000
+  await app.listen(port);
+  logger.log(`App started at port ${port}`)
 }
 bootstrap();
